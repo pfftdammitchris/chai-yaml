0912e236a6bff092d11eddd14646911dc295f223
Christopher Tran <pfftdammitchris@gmail.com>
2022-05-21 15:51:10 -0700

    added value assertions for null, undefined, eq. added support for Pair, YAMLMap, YAMLSeq

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 /// <reference path="./global.d.ts" />
 const Chai = require('chai')
 const y = require('yaml')
+const isEqual = require('lodash/isEqual')
 
 /**
- * Unwraps a YAML Scalar or Document node
- * @param node YAML Node
- * @returns The unwrapped value from node if Scalar or YAML Document. Otherwise the original argument is returned
+ * @typedef YAMLNode
+ * @type { y.Node | y.Scalar | y.Pair | y.YAMLMap | y.YAMLSeq | y.Document }
  */
-function unwrap(node) {
-  if (node !== null && typeof node === 'object') {
-    if (y.isScalar(node)) return node.value
-    if (y.isDocument(node)) return unwrap(node.contents)
-  }
-  return node
-}
 
+const isObj = (o) => o != null && typeof o === 'object'
+
+const { isNode, isScalar, isPair, isMap, isSeq, isDocument: isDoc } = y
+
+/** @returns { node is YAMLNode } */
 function isYamlNode(node) {
-  return y.isNode(node) || y.isDocument(node) || y.isPair(node)
+  return [isNode, isDoc, isPair].some((fn) => fn.call(this, node))
 }
 
 function getNodeType(node) {
-  if (y.isScalar(node)) return 'Scalar'
-  if (y.isPair(node)) return 'Pair'
-  if (y.isMap(node)) return 'Map'
-  if (y.isSeq(node)) return 'Seq'
-  if (y.isDocument(node)) return 'Document'
+  if (isScalar(node)) return 'Scalar'
+  if (isPair(node)) return 'Pair'
+  if (isMap(node)) return 'Map'
+  if (isSeq(node)) return 'Seq'
+  if (isDoc(node)) return 'Document'
   return undefined
+}
+
+function pointToNodeValue(node) {
+  if (isScalar(node)) return node.value
+  if (isPair(node)) return node.value
+  if (isMap(node)) return node
+  if (isSeq(node)) return node
+  if (isDoc(node)) return node.contents
+}
+
+function getJsValue(node) {
+  if (isScalar(node)) return node.toJSON()
+  if (isPair(node)) return getJsValue(node.value)
+  if (isMap(node)) return node.toJSON()
+  if (isSeq(node)) return node.toJSON()
+  if (isDoc(node)) return node.toJSON()
+  return node
 }
 
 /**
@@ -36,91 +51,110 @@ function YamlChai(chai, utils) {
   const Assertion = chai.Assertion
   const expect = chai.expect
 
-  const yamlFlag = function (property, value) {
-    if (value) utils.flag(this, `yaml.${property}`, value)
-    else utils.flag(this, `yaml.${property}`)
+  const setNodeFlags = function (property, value) {
+    if (arguments.length > 1) {
+      utils.flag(this, `yaml.${property}`, value)
+    } else if (isObj(property)) {
+      Object.entries(property).forEach(([key, value]) =>
+        utils.flag(this, `yaml.${key}`, value),
+      )
+    } else {
+      utils.flag(this, `yaml.${property}`)
+    }
   }
 
-  const yamlFlags = function (values) {
-    if (values) {
-      for (const [flag, value] of Object.entries(values)) {
-        yamlFlag(flag, value)
-      }
-    } else {
-      return {
-        node: utils.flag(this, 'yaml.node'),
-        property: utils.flag(this, 'yaml.property'),
-      }
+  const getNodeFlags = function () {
+    return {
+      node: utils.flag(this, 'yaml.node'),
+      property: utils.flag(this, 'yaml.property'),
     }
   }
 
   /**
-   * @this { Chai.AssertionStatic }
+   * @param { string } property
+   * @param { YAMLNode } node
+   * @param { any } value
    */
-  const yamlAssert = function (cond, msg1, msg2, exp, act) {
-    this.assert(cond, msg1, msg2, exp, act)
-  }
-
-  function createAssertNode(Instance) {
-    return (node) => expect(node).to.be.instanceOf(Instance)
-  }
-
-  const assertIs = {
-    Scalar: createAssertNode(y.Scalar),
-    Pair: createAssertNode(y.Pair),
-    YAMLMap: createAssertNode(y.YAMLMap),
-    YAMLSeq: createAssertNode(y.YAMLSeq),
-    Document: createAssertNode(y.Document),
+  function assertProperty(property, node, value) {
+    const actualValue = getJsValue(node)
+    this.assert(
+      isEqual(actualValue, value),
+      `expected #{this} to have ${property} #{exp} but received #{act}`,
+      `expected #{this} to not have ${property} #{exp} but received #{act}`,
+      value,
+      actualValue,
+    )
   }
 
   /**
    *
    * @param { string } property
-   * @param { (this: Chai.AssertionStatic, node: N) => void } fn
+   * @param { (args: { _super: any, node: YAMLNode; flags: { node: string; property: string }; value: any; assertProperty: typeof assertProperty }) => void } fn
    */
-  function yamlProperty(property, fn) {
-    Assertion.addProperty(property, function () {
-      const node = this._obj
-      fn.call(this, node)
-    })
-  }
-
-  /**
-   *
-   * @param { string } method
-   * @param { (this: Chai.AssertionStatic, node: any, ...args: any[]) => void } fn
-   */
-  function yamlMethod(method, fn) {
-    Assertion.addMethod(method, function (...args) {
-      const node = this._obj
-      fn.call(this, node, ...args)
-    })
-  }
-
   function overwriteProperty(property, fn) {
     Assertion.overwriteProperty(property, function (_super) {
       return function () {
-        return fn.call(this, _super, this._obj)
+        return fn.call(this, {
+          _super,
+          flags: getNodeFlags(),
+          node: this._obj,
+          assertProperty: assertProperty.bind(this),
+        })
       }
     })
   }
 
-  overwriteProperty('deep', function (_super, node) {
-    const flags = yamlFlags()
-    if (isYamlNode(node) && flags.property === 'value') {
-      yamlFlags({ node: getNodeType(node) })
+  /**
+   * @param { string } method
+   * @param { (args: { _super: any, node: YAMLNode; flags: { node: string; property: string }; value: any; assertProperty: typeof assertProperty }) => void } fn
+   */
+  function overwriteMethod(method, fn) {
+    Assertion.overwriteMethod(method, function (_super) {
+      return function (value) {
+        const node = this._obj
+        const flags = getNodeFlags()
+        fn.call(this, {
+          _super,
+          node,
+          flags,
+          value,
+          assertProperty: assertProperty.bind(this),
+        })
+      }
+    })
+  }
+
+  overwriteProperty('value', function ({ _super, node }) {
+    if (isYamlNode(node)) {
+      setNodeFlags('node', getNodeType(node))
+      setNodeFlags('property', 'value')
     } else {
       _super.call(this)
     }
   })
 
-  overwriteProperty('null', function (_super, node) {
-    const flags = yamlFlags()
-    if (isYamlNode(node) && flags.property === 'value') {
-      if (flags.node === 'Scalar') {
-        new Assertion(node.value).to.be.null
-      } else if (flags.node === 'Document') {
-        new Assertion(node.contents).to.be.null
+  overwriteMethod(
+    'eq',
+    function ({ _super, node, flags, value, assertProperty }) {
+      const eqFlagKeys = ['value']
+      if (flags.node) {
+        if (flags.property.includes(eqFlagKeys)) {
+          eqFlagKeys.forEach((eqFlagKey) => {
+            assertProperty(eqFlagKey, pointToNodeValue(node), value)
+          })
+        } else {
+          _super.apply(this, arguments)
+        }
+      } else {
+        _super.apply(this, arguments)
+      }
+    },
+  )
+
+  overwriteProperty('null', function ({ _super, assertProperty, flags, node }) {
+    if (isYamlNode(node)) {
+      if (flags.property === 'value') {
+        assertProperty('value', pointToNodeValue(node), null)
       } else {
         _super.call(this)
       }
@@ -129,117 +163,20 @@ function YamlChai(chai, utils) {
     }
   })
 
-  overwriteProperty('value', function (_super, node) {
-    if (isYamlNode(node)) {
-      yamlFlags({ node: getNodeType(node), property: 'value' })
-    } else {
-      _super.call(this)
-    }
-  })
-
-  Assertion.overwriteMethod('eq', function (_super) {
-    return function (value) {
-      const node = this._obj
-      const flags = yamlFlags()
-
-      console.log(`[eq]`, { flags, contents: node.contents, value })
-
-      if (flags.property === 'value') {
-        if (flags.node === 'Scalar') {
-          new Assertion(node).to.have.property('value', value)
-        } else if (flags.node === 'Document') {
-          if (!node.contents) {
-            this.assert(
-              node.contents === value,
-              `expected #{this} to have value #{exp} but received #{act}`,
-              `expected #{this} to not have value #{exp} but received #{act}`,
-              value,
-              node.contents,
-            )
-          } else {
-            if (y.isMap(node.contents)) {
-              if (isYamlNode(value)) {
-                expect(node.contents.toJSON()).to.deep.eq(value)
-                // this.assert(
-                //   node.contents.toJSON() === value,
-                //   `expected #{this} to have value #{exp}`,
-                //   `expected #{this} to not have value #{exp}`,
-                //   value,
-                //   node.contents.toJSON(),
-                // )
-              } else {
-                expect(node.contents.toJSON()).to.deep.eq(value)
-              }
-
-              // this.assert(
-              //   unwrap(node.contents) === value,
-              //   `expected #{this} to have value #{exp}`,
-              //   `expected #{this} to not have value #{exp}`,
-              //   value,
-              //   node.contents,
-              // )
-            } else if (y.isScalar(node.contents)) {
-              this.assert(
-                unwrap(node.contents) === value,
-                `expected #{this} to have value #{exp} but received #{act}`,
-                `expected #{this} to not have value #{exp} but received #{act}`,
-                value,
-                unwrap(node.contents),
-              )
-            } else {
-              _super.apply(this, arguments)
-            }
-          }
+  overwriteProperty(
+    'undefined',
+    function ({ _super, assertProperty, flags, node }) {
+      if (isYamlNode(node)) {
+        if (flags.property === 'value') {
+          assertProperty('value', pointToNodeValue(node), undefined)
+        } else {
+          _super.call(this)
         }
       } else {
-        _super.apply(this, arguments)
+        _super.call(this)
       }
-    }
-  })
-
-  // yamlMethod('value', function (node, value) {
-  //   console.log({ node, value })
-  //   if (y.isMap(node)) {
-  //     this.assert(
-  //       node.items.some((pair) => {
-  //         if (isYAMLNode(pair.value)) return is.equalTo(pair.value, value)
-  //         return unwrap(pair.value) === value
-  //       }),
-  //       `expected #{this} to have value #{exp}`,
-  //       `expected #{this} to not have value #{exp}`,
-  //       value,
-  //     )
-  //   }
-
-  //   if (y.isPair(node)) {
-  //     this.assert(
-  //       unwrap(node.value) === value,
-  //       `expected Pair #{this} to have value #{exp} but got #{act}`,
-  //       `expected Pair #{this} to not have value #{exp} but got #{act}`,
-  //       value,
-  //       node.value,
-  //     )
-  //   }
-
-  //   if (y.isScalar(node)) {
-  //     this.assert(
-  //       unwrap(node) === value,
-  //       `expected #{this} to have value #{exp} but got #{act}`,
-  //       `expected #{this} to not have value #{exp} but got #{act}`,
-  //       value,
-  //       node.value,
-  //     )
-  //   }
-
-  //   if (y.isSeq(node)) {
-  //     this.assert(
-  //       unwrap(node.get(value)) === value,
-  //       `expected YAMLSeq #{this} to have value #{exp}`,
-  //       `expected YAMLSeq #{this} to not have value #{exp}`,
-  //       value,
-  //     )
-  //   }
-  // })
+    },
+  )
 }
 
 module.exports = YamlChai
--- index.test.js
+++ index.test.js
@@ -2,34 +2,140 @@
 
 const y = require('yaml')
 const chai = require('chai')
-const AssertionError = require('chai').AssertionError
 const expect = chai.expect
 const chaiYaml = require('.')
 
 chai.use(chaiYaml)
 
-describe(`Scalar`, () => {
-  it(`should be true when value matches`, () => {
-    process.stdout.write('\x1Bc')
-    const node = new y.Scalar('hello')
-    // expect(node).scalar.value.to.eq('hello')
-    // expect(node).value.to.eq('f')
-    expect(new y.Document()).value.to.eq('f')
+describe(`value eq`, () => {
+  describe(`Scalar`, () => {
+    it(`should equal value`, () => {
+      expect(new y.Scalar('hello')).value.to.eq('hello')
+      expect(new y.Scalar(null)).value.to.eq(null)
+      expect(new y.Scalar(undefined)).value.to.eq(undefined)
+      expect(new y.Scalar('')).value.to.eq('')
+      expect(new y.Scalar('')).value.to.eq('')
+    })
+  })
+
+  describe(`Pair`, () => {
+    it(`should equal value`, () => {
+      expect(new y.Pair('hi', 0)).value.to.eq(0)
+      expect(new y.Pair('hi', 1)).value.to.eq(1)
+      expect(new y.Pair('hi', null)).value.to.eq(null)
+      expect(new y.Pair('hi', 'hello')).value.to.eq('hello')
+      expect(new y.Pair('hi', 'hi')).value.to.eq('hi')
+      expect(new y.Pair('hi', '0')).value.to.eq('0')
+      expect(new y.Pair('hi', '1')).value.to.eq('1')
+      // expect(new y.Pair('hi', undefined)).value.to.eq(undefined)
+      // expect(new y.Pair('hi')).value.to.eq(undefined)
+    })
+  })
+
+  describe(`Map`, () => {
+    it(`should equal value`, () => {
+      const node = new y.YAMLMap()
+      node.set('hi', 'hello')
+      expect(node).value.to.deep.eq({ hi: 'hello' })
+      node.set('hi2', 'hello')
+      expect(node).value.not.to.deep.eq({ hi: 'hello' })
+      expect(node).value.to.deep.eq({ hi2: 'hello', hi: 'hello' })
+      node.delete('hi')
+      expect(node).value.to.deep.eq({ hi2: 'hello' })
+      node.delete('hi2')
+      expect(node).value.to.deep.eq({})
+    })
+  })
+
+  describe(`Seq`, () => {
+    const node = new y.YAMLSeq()
+    expect(node).value.to.deep.eq([])
+    node.set(0, 'hello')
+    expect(node).value.to.deep.eq(['hello'])
+    node.set(0, 'hi')
+    expect(node).value.to.deep.eq(['hi'])
+    node.add('hello2')
+    expect(node).value.to.deep.eq(['hi', 'hello2'])
+    expect(node.items).to.have.lengthOf(2)
+    node.items.length = 0
+    expect(node).value.to.deep.eq([])
+  })
+
+  describe(`Document`, () => {
+    it(`should equal value of document contents`, () => {
+      expect(new y.Document(true)).value.to.eq(true)
+      expect(new y.Document(false)).value.to.eq(false)
+      expect(new y.Document('hello')).value.to.eq('hello')
+      expect(new y.Document(11)).value.to.eq(11)
+      expect(new y.Document({})).value.to.deep.eq({})
+      expect(new y.Document([])).value.to.deep.eq([])
+      expect(new y.Document([''])).value.to.deep.eq([''])
+    })
   })
 })
 
-describe(`Document`, () => {
-  it.only(`should match value of document contents`, () => {
-    expect(new y.Document()).value.to.be.null
-    expect(new y.Document('hello')).value.to.eq('hello')
-    expect(new y.Document(11)).value.to.eq(11)
-    console.log(new y.Document({}))
-    expect(new y.Document({})).value.to.deep.eq({})
+describe(`value null`, () => {
+  describe(`Scalar`, () => {
+    it(`should be null`, () => {
+      expect(new y.Scalar(null)).value.to.be.null
+    })
+  })
+
+  describe(`Document`, () => {
+    it(`should be null`, () => {
+      expect(new y.Document()).value.to.be.null
+      expect(new y.Document(null)).value.to.be.null
+    })
+  })
+
+  describe(`Pair`, () => {
+    it(`should be null`, () => {
+      expect(new y.Pair('hello', null)).value.to.be.null
+    })
+  })
+
+  describe(`YAMLMap`, () => {
+    it(`should not be null`, () => {
+      expect(new y.YAMLMap()).value.not.to.be.null
+    })
+  })
+
+  describe(`YAMLSeq`, () => {
+    it(`should not be null`, () => {
+      expect(new y.YAMLSeq()).value.not.to.be.null
+    })
   })
 })
 
-describe(`pair`, () => {})
+describe(`value undefined`, () => {
+  describe(`Scalar`, () => {
+    it(`should be undefined`, () => {
+      expect(new y.Scalar(undefined)).value.to.be.undefined
+      expect(new y.Scalar()).value.to.be.undefined
+    })
+  })
 
-describe(`map`, () => {})
+  describe(`Document`, () => {
+    xit(`should be undefined`, () => {
+      expect(new y.Document(undefined)).value.to.be.undefined
+    })
+  })
 
-describe(`seq`, () => {})
+  describe(`Pair`, () => {
+    it(`should be undefined`, () => {
+      expect(new y.Pair('hello', null)).value.to.be.null
+    })
+  })
+
+  describe(`YAMLMap`, () => {
+    it(`should not be undefined`, () => {
+      expect(new y.YAMLMap()).value.not.to.be.undefined
+    })
+  })
+
+  describe(`YAMLSeq`, () => {
+    it(`should not be undefined`, () => {
+      expect(new y.YAMLSeq()).value.not.to.be.undefined
+    })
+  })
+})
